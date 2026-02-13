2fd15dbbde52b9977a95b6604c6ec37265f57bd0
sbnm007 <sibin.georgge@gmail.com>
2026-02-13 01:56:36 +0000

    deploy changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/sbnm007/website.git',
   server: {
     port: 3000,
     open: true
